46f63615317cc10583df011af0b43f72afac600b
rodmartinezmedina <rodmartinezmedina@gmail.com>
2020-04-19 18:26:47 +0200

    make folder and file structure on Client folder for writing auth and everything else

--- pm-Client/src/index.js
+++ pm-Client/src/index.js
@@ -1,18 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { AuthProvider } from "./lib/Auth";
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-import { BrowserRouter as Router } from 'react-router-dom';
+
+import './index.css';
+
 
 ReactDOM.render((
-<Router> 
+<Router>
+  <AuthProvider>
     <App />
+  </AuthProvider>   
 </Router> 
 ), document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+// Learn more about service workers: https://bit.ly/CRA-PWA<App />
 serviceWorker.unregister();
