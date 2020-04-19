ededbe782326892e49df51b24730b3297a900f2e
rodmartinezmedina <rodmartinezmedina@gmail.com>
2020-04-19 16:19:20 +0200

    ON SERVER: create auth.js route. finish user-model.js

--- pm-Server/routes/auth.js
+++ pm-Server/routes/auth.js
@@ -15,4 +15,99 @@ const {
 
 
 //POST '/auth/signup'
-router.post
+router.post('/signup', isNotLoggedIn, validationLogin, async (req, res, next) => {
+  cont { username, password, email } = req.body;
+
+  try {
+    const usernameExists = await User.findOne( { username },'username');
+
+    if (usernameExists) return next(createError(400));
+    else {
+      const salt = bcrypt.genSaltSync(saltRounds);
+      const hashPass = bcrypt.hashSync(password, salt);
+      const newUser = await User.create({ username, password,hashPass, email });
+
+      newUser.password = "***";
+      req.session.currentUser = newUser;
+      res
+        .status(201) // Created user
+        .json(newUser);
+    }
+  }
+  catch (error) {
+    next(createError(error));
+  }
+},
+);
+
+// POST '/auth/login'
+router.post('/login', isNotLoggedIn, validationLogin, async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ email:email }) ;
+    if (!user) {
+      next(createError(404));
+    }
+    else if (bcrypt.compareSync(password, user.password)) {
+      
+      user.password = '****';
+      req.session.currentUser = user;
+      res
+        .status(200)
+        .json(user);
+
+    //return; 
+    }
+    else {
+      next(createError(401)); // Unauthorized
+    }
+  }
+  catch (error) {
+    next(createError(error));
+  }
+},
+);
+
+
+// POST '/auth/logout;
+router.post('/logout', isLoggedIn, (req, res, next) => {
+  req.session.destroy();
+  res
+    .status(204) // 
+    .send();
+});
+
+
+// GET '/auth/me'
+router.get('/me', isLoggedIn, (req, res, next) => {
+  const currentUserSessionData = req.session.currentUser;
+  currentUserSessionData.password = '****';
+
+  res.status(200).json(currentUserSessionData);
+});
+
+router.post("/edit" , isLoggedIn, (req, res, next) => {
+  const { username, email, password } = req.body;
+  const {id} = req.sesssion.currentUser;
+  User.findByIdAndUpdate({id}, {$set : { username, email, password }}, {new:true} )
+  .then((updatedUser) => {
+    res
+    .status(200)
+    .json(updatedUser)
+  })
+  .catch((err) => {
+    res
+    .status(500)
+    .json(err);
+  });
+})
+
+//GET '/auth/admin'
+router.get('/admin', isLoggedIn, isAdmin, (req, res next) => {
+  const currentUserSessionData = req.session.currentUser;
+  currentUserSessionData.password = '****';
+
+  res.status(200).json(currentUserSessionData);
+});
+
+module.exports = router;
